3d2d66fc127a732518e5802777327cab36e68e9f
kujiraitakahiro <takahiro.kujirai@it.zeus-enterprise.co.jp>
2018-05-07 17:30:29 +0900

    First commit

---
+++ graph1.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+# -*- coding: utf-8 -*-
+
+driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
+
+def print_name(tx, name):
+    for record in tx.run("MATCH (a:Person)-[:ACTED_IN]->(movie) WHERE a.name = $name "
+                         "RETURN movie.name ORDER BY movie.name", name=name):
+        print(record["movie.name"])
+
+with driver.session() as session:
+    session.read_transaction(print_name, "The Matrix")
+
+

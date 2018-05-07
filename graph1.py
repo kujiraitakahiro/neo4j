59063f4e46e9cd767e95b17f77d5b1d08c934d6c
kujiraitakahiro <takahiro.kujirai@it.zeus-enterprise.co.jp>
2018-05-07 18:06:41 +0900

    5th commit

--- graph1.py
+++ graph1.py
@@ -6,7 +6,7 @@ from neo4j.v1 import GraphDatabase
 driver = GraphDatabase.driver("bolt://localhost:7687", auth=("neo4j", "password"))
 
 def print_name(tx, name):
-    for record in tx.run("MATCH (a:Person)-[:ACTED_IN]->(movie) WHERE a.name = $name "
+    for record in tx.run("MATCH (a)-[:ACTED_IN]->(m)<-[:DIRECTED]-(d) WHERE a.name = $name "
                          "RETURN a.name ORDER BY a.name", name=name):
         print(record["a.name"])
 
